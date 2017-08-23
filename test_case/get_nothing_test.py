a76b8803cfc2f3a5f1c58780be2e6f9ff1560ca2
金宗杰 <hyuhyu2001@163.com>
2017-08-23 15:00:47 +0800

    20170823

--- test_case/get_nothing_test.py
+++ test_case/get_nothing_test.py
@@ -9,26 +9,31 @@ import requests
 import json
 import unittest
 from public import base
+from ddt import ddt,data,unpack
 
+@ddt
 class GetNothingTest(unittest.TestCase):
     '''GET无参数测试'''
     def setUp(self):
         endpoint = 'get'
         self.url = base.get_url(endpoint)
 
-    def test_1(self):
+    @data(200,400,500,201,301)
+    def test_1(self,result):
         '''校验状态码是否为200'''
         r = requests.get(self.url)
 
         status_code  = r.status_code
-        self.assertEqual(status_code,200)
+        self.assertEqual(status_code,result)
 
-    def test_2(self):
+    @data(['headers','Connection','close'],['headers','Host','httpbin.org'])
+    @unpack
+    def test_2(self,headers,value,result):
         '''校验headers里的Connection值'''
         r = requests.get(self.url)
         resp = r.json()
-        connect = resp['headers']['Connection']
-        self.assertEqual(connect, 'close')
+        connect = resp[headers][value]
+        self.assertEqual(connect,result)
 
     def tearDown(self):
         pass
